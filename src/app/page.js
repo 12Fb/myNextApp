764f628082b3687403ac1bd38d44c42ae0c5f0ef
黄博远 <david.2003@foxmail.com>
2024-05-14 19:26:31 +0800

    feat: 基本布局，滚动条全局样式

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,50 @@
 import styles from "./page.module.css";
-import homeCss from "./home.css";
 import Link from "next/link";
 export default function Home() {
-  const routerArr = [{ path: "/about" }];
+  const routerArr = [
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about" },
+    { path: "/about1" },
+    { path: "/about1" },
+    { path: "/about1" },
+    { path: "/about" },
+  ];
   return (
     <>
-      <div className="Container">
-        {routerArr.map((item) => (
-          <div className="linkContainer">
+      <div>
+        {routerArr.map((item, index) => (
+          <div>
             <Link href={item.path} key={item.path}>
               {item.path}
             </Link>
